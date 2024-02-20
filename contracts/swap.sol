29635358b5a5d8569ebd717bf33604638ada4ca1
mubarakOke <okeolamubarak18@gmail.com>
2024-02-20 13:52:30 +0100

    integrated interface

--- contracts/swap.sol
+++ contracts/swap.sol
@@ -1,18 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import './IERC20.sol';
+
 contract Swap {
     address token1Address;
     address token2Address;
     event Swapped(address indexed _user, uint256 _amount);
 
-    constructor(token){
+    error
 
+    constructor(addres _token1Address, address _token2Address){
+        token1Address= _token1Address;
+        token2Address= _token2Address;
     }
 
     function swapAforB(uint256 _amount ) returns(bool){
         require(msg.sender != address(0));
-        require()
+        require(IERC20())
     }
 
     function swapBforA(uint256 _amount) returns(bool){
